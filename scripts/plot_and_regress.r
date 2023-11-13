ec9dc097778ea39b06e762f5340ac7d94a39e0f7
Connor Mitchell <143525419+connormll@users.noreply.github.com>
2023-11-13 18:37:00 -0500

    Update plot_and_regress.r

--- scripts/plot_and_regress.r
+++ scripts/plot_and_regress.r
@@ -1,42 +1,50 @@
 library(ggplot2)
 library(tidyverse)
 library(readxl)
-library(ggstatsplot)
-library(lm)
-library(hexbin)
+
 
 #read in data
-data_sorted <- read_excel("/data/excel.xlsx", sheet = "Sorted")
+data_sorted <- read_excel("~/Github/Honors-Thesis-ADA-project/data/excel.xlsx", 
+                          sheet = "Sorted", col_types = c("numeric", 
+                                                          "text", "date", "text", "numeric", 
+                                                          "numeric", "numeric", "numeric", 
+                                                          "numeric", "numeric", "numeric", 
+                                                          "numeric", "numeric", "numeric", 
+                                                          "numeric", "numeric", "numeric", 
+                                                          "numeric", "numeric", "numeric", 
+                                                          "numeric", "numeric", "numeric", 
+                                                          "numeric", "numeric", "numeric", 
+                                                          "numeric", "numeric", "numeric", 
+                                                          "numeric", "numeric", "numeric", 
+                                                          "numeric", "numeric", "numeric", 
+                                                          "numeric", "numeric", "numeric", 
+                                                          "numeric", "numeric", "numeric", 
+                                                          "numeric", "numeric", "numeric", 
+                                                          "numeric", "numeric", "numeric", 
+                                                          "numeric", "numeric", "numeric", 
+                                                          "numeric", "numeric", "numeric", 
+                                                          "numeric", "numeric", "numeric", 
+                                                          "numeric", "numeric", "numeric", 
+                                                          "numeric", "numeric", "numeric", 
+                                                          "numeric", "numeric", "numeric", 
+                                                          "numeric", "numeric", "numeric", 
+                                                          "numeric", "numeric", "numeric", 
+                                                          "numeric", "numeric", "numeric", 
+                                                          "numeric", "numeric", "numeric"))
 
 #gather names for for loop
-column_names <- names(data_sorted)[-1]
-
-mass_graph <- ggplot(
-  data = data_sorted,
-  aes(
-    x = Date,
-    y = ,
-  )
-) +
-  stat_binhex() +
-  scale_fill_gradiant(
-    low = "lightblue",
-    high = "red",
-    limits = c(0,)
-  )
-ggsave(filename = "data/output/ggplots/main.png", plot = mass_graph)
+column_names <- names(data_sorted)[5:77]
+date_column <- data_sorted[[3]]
 
 for (col_name in column_names) {
-  p <- ggplot(
-    data = data_sorted,
-    aes(
-      x = "Date",
-      y = col_name
-    )
-  )
-  p +
-    geom_point(colour = "grey60") +
-    stat_smoth(method = lm, colour = "black")
+  p <- ggplot(data = data_sorted,aes_string(x = names(data_sorted)[3], y = col_name)) +
+    geom_point() +
+    theme_bw()+
+    geom_smooth(method=lm, color="red", fill="#69b3a2", se=TRUE) +
+    ylim(0,1)
+    labs(title = paste("Probability plot for topic #", col_name), 
+         x = "Time",
+         y = "Probability")
   ggsave(
-    filename = paste0("/data/output/ggplots/plot_", col_name, ".png"), plot = p)
+    filename = paste0("~/Github/Honors-Thesis-ADA-project//data/output/ggplots/plot_", col_name, ".png"), plot = p)
 }
